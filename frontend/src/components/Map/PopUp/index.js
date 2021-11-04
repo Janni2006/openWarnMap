f34fbbfa1e03003a78bcee261e84bc5befa2c692
Jan-Niklas Wielpütz <janni.wielpuetz@gmail.com>
2021-11-04 11:17:45 +0100

    PopUp Accordion

--- frontend/src/components/Map/PopUp/index.js
+++ frontend/src/components/Map/PopUp/index.js
@@ -6,14 +6,20 @@ import { motion, AnimateSharedLayout } from "framer-motion";
 
 import { closeMarkerPopup } from "../../../actions/mapActions";
 
-import { VerifiedUser, Warning, Close } from "@material-ui/icons";
+import { VerifiedUser, Warning, Close, ExpandMore } from "@material-ui/icons";
 
 import {
 	Typography,
 	IconButton,
 	withWidth,
 	isWidthDown,
+	withStyles,
+	Divider,
 } from "@material-ui/core";
+
+import MuiAccordion from "@material-ui/core/Accordion";
+import MuiAccordionSummary from "@material-ui/core/AccordionSummary";
+import MuiAccordionDetails from "@material-ui/core/AccordionDetails";
 
 import Votes from "./Votes";
 
@@ -22,9 +28,142 @@ import getDistance from "geolib/es/getDistance";
 
 //  Add details accordion
 
+const Accordion = withStyles({
+	root: {
+		// border: "1px solid rgba(0, 0, 0, .125)",
+		boxShadow: "none",
+		"&:not(:last-child)": {
+			borderBottom: 0,
+		},
+		"&:before": {
+			display: "none",
+		},
+		"&$expanded": {
+			margin: "auto",
+		},
+	},
+	expanded: {},
+})(MuiAccordion);
+
+const AccordionSummary = withStyles({
+	root: {
+		// backgroundColor: "rgba(0, 0, 0, .03)",
+		// borderBottom: "1px solid rgba(0, 0, 0, .125)",
+		marginBottom: -1,
+		padding: "0px",
+		minHeight: 24,
+		"&$expanded": {
+			minHeight: 24,
+		},
+	},
+	content: {
+		"&$expanded": {
+			margin: "0px",
+		},
+	},
+	expanded: {},
+})(MuiAccordionSummary);
+
+const AccordionDetails = withStyles((theme) => ({
+	root: {
+		padding: "0px 0px 12px 0px",
+	},
+}))(MuiAccordionDetails);
+
+function I18nHeight(props) {
+	switch (props.height) {
+		case 0:
+			return (
+				<Typography>
+					<FormattedMessage id="ADD_HEIGHT" />:{" "}
+					<FormattedMessage id="ADD_HEIGHT_OPTION_1" />
+				</Typography>
+			);
+		case 1:
+			return (
+				<Typography>
+					<FormattedMessage id="ADD_HEIGHT" />:{" "}
+					<FormattedMessage id="ADD_HEIGHT_OPTION_2" />
+				</Typography>
+			);
+		case 2:
+			return (
+				<Typography>
+					<FormattedMessage id="ADD_HEIGHT" />:{" "}
+					<FormattedMessage id="ADD_HEIGHT_OPTION_3" />
+				</Typography>
+			);
+		default:
+			return (
+				<Typography>There was an error while gethering the height</Typography>
+			);
+	}
+}
+
+function I18nSize(props) {
+	switch (props.size) {
+		case 0:
+			return (
+				<Typography>
+					<FormattedMessage id="ADD_SIZE" />:{" "}
+					<FormattedMessage id="ADD_SIZE_OPTION_1" />
+				</Typography>
+			);
+		case 1:
+			return (
+				<Typography>
+					<FormattedMessage id="ADD_SIZE" />:{" "}
+					<FormattedMessage id="ADD_SIZE_OPTION_2" />
+				</Typography>
+			);
+		case 2:
+			return (
+				<Typography>
+					<FormattedMessage id="ADD_SIZE" />:{" "}
+					<FormattedMessage id="ADD_SIZE_OPTION_3" />
+				</Typography>
+			);
+		default:
+			return (
+				<Typography>There was an error while gethering the size</Typography>
+			);
+	}
+}
+
+function I18nLocalization(props) {
+	switch (props.localization) {
+		case 0:
+			return (
+				<Typography>
+					<FormattedMessage id="ADD_LOCALIZATION" />:{" "}
+					<FormattedMessage id="ADD_LOCALIZATION_OPTION_1" />
+				</Typography>
+			);
+		case 1:
+			return (
+				<Typography>
+					<FormattedMessage id="ADD_LOCALIZATION" />:{" "}
+					<FormattedMessage id="ADD_LOCALIZATION_OPTION_2" />
+				</Typography>
+			);
+		case 2:
+			return (
+				<Typography>
+					<FormattedMessage id="ADD_LOCALIZATION" />:{" "}
+					<FormattedMessage id="ADD_LOCALIZATION_OPTION_3" />
+				</Typography>
+			);
+		default:
+			return (
+				<Typography>There was an error while gethering the position</Typography>
+			);
+	}
+}
+
 function MarkerPopup(props) {
 	const [zIndex, setZIndex] = React.useState("auto");
 	const [distance, setDistance] = React.useState(null);
+	const [expanded, setExpanded] = React.useState(false);
 
 	var old_code;
 
@@ -77,6 +216,10 @@ function MarkerPopup(props) {
 			}, 250);
 		}
 	}, [props.open]);
+
+	const handleChange = (panel) => (event, newExpanded) => {
+		setExpanded(newExpanded ? panel : false);
+	};
 
 	return (
 		<>
@@ -165,18 +308,39 @@ function MarkerPopup(props) {
 									</Typography>
 								</div>
 							) : null}
-							<Typography>Höhe: {props.content.height}</Typography>
-							<Typography>Größe: {props.content.size}</Typography>
-							<Typography>Position: {props.content.localization}</Typography>
-							{distance != null && props.gpsAvailable ? (
-								<Typography>
-									Distance:{" "}
-									{distance >= 1000
-										? Math.round(distance / 100) / 10
-										: distance}
-									{distance >= 1000 ? "km" : "m"}
-								</Typography>
-							) : null}
+							<Accordion
+								square
+								expanded={expanded === "details_panel"}
+								onChange={handleChange("details_panel")}
+							>
+								<AccordionSummary
+									expandIcon={<ExpandMore />}
+									aria-controls="details_panel-content"
+									id="details_panel-header"
+								>
+									Details
+									<Divider flexItem />
+								</AccordionSummary>
+								<AccordionDetails>
+									<div style={{ display: "block" }}>
+										<I18nHeight height={props.content.height} />
+										<I18nSize size={props.content.size} />
+										<I18nLocalization
+											localization={props.content.localization}
+										/>
+										{distance != null && props.gpsAvailable ? (
+											<Typography>
+												Distance:{" "}
+												{distance >= 1000
+													? Math.round(distance / 100) / 10
+													: distance}
+												{distance >= 1000 ? "km" : "m"}
+											</Typography>
+										) : null}
+									</div>
+								</AccordionDetails>
+							</Accordion>
+
 							<Votes />
 						</motion.div>
 					) : (
