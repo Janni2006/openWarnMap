a15aae9de2eb4ebea1f5a9706e2301035dc1885b
Jan-Niklas Wielpütz <janni.wielpuetz@gmail.com>
2022-11-11 22:53:23 +0100

    update all dependecies (pip)

--- frontend/src/components/PublicProfile.js
+++ frontend/src/components/PublicProfile.js
@@ -1,12 +1,86 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+
+import { withRouter, useLocation, Link } from "react-router-dom";
+
 import { setTitle } from "../actions/generalActions";
 
-import { Grid, Paper, Typography } from "@mui/material";
+import {
+	List,
+	Grid,
+	ListItem,
+	Avatar,
+	Typography,
+	Hidden,
+} from "@mui/material";
+
+import { useIntl } from "react-intl";
+
+import { motion, AnimateSharedLayout } from "framer-motion";
+
+function SiteItem({ isSelected, icon, title, label, username }) {
+	return (
+		<Link
+			to={`/user/${username}?page=${label}`}
+			style={{ width: "100%", padding: "0px", textDecoration: "none" }}
+		>
+			<ListItem
+				style={{
+					position: "relative",
+					padding: "10px",
+					height: "44px",
+					borderTop: "1px solid #f0f0f0",
+				}}
+			>
+				<Grid
+					container
+					style={{
+						zIndex: 1,
+						height: "24px",
+					}}
+					spacing={1}
+				>
+					<Hidden lgDown>
+						<Grid item xs={3} style={{ color: "#3f3f3f" }}>
+							{icon}
+						</Grid>
+					</Hidden>
+
+					<Hidden smUp>
+						<Grid item xs={3} style={{ color: "#3f3f3f" }}>
+							{icon}
+						</Grid>
+					</Hidden>
+
+					<Grid item xs={9}>
+						<Typography style={{ color: "#3f3f3f" }}>{title}</Typography>
+					</Grid>
+				</Grid>
+				{isSelected ? (
+					<motion.div
+						layoutId="selector"
+						style={{
+							position: "absolute",
+							top: "0px",
+							left: "0px",
+							height: "44px",
+							backgroundColor: "#f0f0f0",
+							borderRight: "2px solid #3f3f3f",
+							width: "calc(100% - 2px)",
+							zIndex: 0,
+						}}
+					/>
+				) : null}
+			</ListItem>
+		</Link>
+	);
+}
 
 function PublicProfile(props) {
 	const [username, setUsername] = React.useState("");
+	const [openedSite, setOpenedSite] = React.useState(0);
+	const intl = useIntl();
 
 	React.useEffect(() => {
 		props.setTitle(`Profile ${username}`);
@@ -16,44 +90,181 @@ function PublicProfile(props) {
 	}, [username]);
 
 	React.useEffect(() => {
-		setUsername(window.location.pathname.replace("/user", "").replace("/", ""));
-	}, [window]);
+		setUsername(props.match.params.user);
+	}, [props.match.params]);
+
+	const pages = [
+		{
+			label: "account",
+			index: 0,
+			icon: (
+				<>
+					<div>Test</div>
+				</>
+			),
+			title: intl.formatMessage({ id: "PROFILE_PAGE_TITLE_ACCOUNT" }),
+			content: () => <></>,
+		},
+		{
+			label: "password",
+			index: 1,
+			icon: (
+				<>
+					<div>Test</div>
+				</>
+			),
+			title: intl.formatMessage({ id: "PROFILE_PAGE_TITLE_PASSWORD" }),
+			content: () => <></>,
+		},
+		// {
+		// 	label: "security",
+		// 	index: 2,
+		// 	icon: <SecurityRounded />,
+		// 	title: intl.formatMessage({ id: "PROFILE_PAGE_TITLE_SECURITY" }),
+		// 	content: () => <Security />,
+		// },
+		{
+			label: "notifications",
+			index: 2,
+			icon: (
+				<>
+					<div>Test</div>
+				</>
+			),
+			title: intl.formatMessage({ id: "PROFILE_PAGE_TITLE_NOTIFICATIONS" }),
+			content: () => <></>,
+		},
+	];
+
+	const search = useLocation().search;
+	const page = new URLSearchParams(search).get("page");
+
+	React.useEffect(() => {
+		if (page) {
+			var req_page = pages.find((item) => item.label == page);
+
+			setOpenedSite(req_page.index);
+		}
+	}, [page]);
 
 	return (
-		<>
-			<Paper
-				style={{
-					maxWidth: "800px",
-					width: "95vw",
-					margin: "auto",
-					marginTop: "8px",
-				}}
-			>
-				<Grid container spacing={2}>
-					<Grid item xs={12} sm={6} style={{ padding: "15px 2.5px 20px 30px" }}>
-						<Typography
-							style={{
-								color: "#FFBD6D",
-								fontSize: "36px",
-								fontWeight: "bold",
-							}}
-						>
-							Open Source
-						</Typography>
-						<Typography style={{ textAlign: "justify" }}>
-							Developers across the world benefit from the Open Source software
-							ecosystem and OpenWeather is not an exception. While influenced by
-							the platforms like Wikipedia and OpenStreetMap, we have used the
-							Open Source software in the foundation of our platform. To inspire
-							the Open Source developers and to facilitate the accessibility of
-							weather data, we choose to gear up those who contribute to the
-							Open Source infrastructure.
-						</Typography>
-					</Grid>
-					<Grid item xs={12} sm={6} style={{ padding: "20px" }}></Grid>
+		<div
+			style={{
+				padding: "22px",
+				maxWidth: "1200px",
+				margin: "auto",
+			}}
+		>
+			<Grid container spacing={4}>
+				<Grid item xs={12} sm={4} md={3}>
+					<motion.div
+						layout
+						style={{
+							margin: "auto",
+							padding: "15px 0px 2px 0px",
+							overflow: "hidden",
+							minWidth: "80px",
+							borderRadius: "5px",
+							backgroundColor: "white",
+							boxShadow: "5px 5px 30px -20px #3f3f3f",
+						}}
+					>
+						<List style={{ padding: "0px" }}>
+							<ListItem>
+								<Avatar
+									style={{
+										border: "solid 2.5px white",
+										boxShadow: `0px 0px 16px -10px ${
+											props.avatar_color ? props.avatar_color : "#bdbdbd"
+										}`,
+										height: "75px",
+										width: "75px",
+										margin: "auto",
+										backgroundColor: props.avatar_color
+											? props.avatar_color
+											: "#bdbdbd",
+									}}
+								>
+									{/* {props.username.charAt(0).toUpperCase()} */}Test
+								</Avatar>
+							</ListItem>
+							<ListItem>
+								<Hidden xlDown>
+									<Typography style={{ textAlign: "center", width: "100%" }}>
+										{props.firstname && props.lastname
+											? `${props.firstname} ${props.lastname}`
+											: props.email}
+									</Typography>
+								</Hidden>
+								<Hidden xlUp mdDown>
+									{props.firstname && props.lastname ? (
+										<>
+											<Typography
+												style={{
+													textAlign: "center",
+													width: "100%",
+													color: "#3f3f3f",
+												}}
+											>
+												{props.firstname}
+											</Typography>
+										</>
+									) : (
+										<Typography
+											style={{
+												textAlign: "center",
+												width: "100%",
+												color: "#3f3f3f",
+											}}
+										>
+											{props.username}
+										</Typography>
+									)}
+								</Hidden>
+								<Hidden smUp>
+									<Typography
+										style={{
+											textAlign: "center",
+											width: "100%",
+											color: "#3f3f3f",
+										}}
+									>
+										{props.firstname && props.lastname
+											? `${props.firstname} ${props.lastname}`
+											: props.email}
+									</Typography>
+								</Hidden>
+							</ListItem>
+
+							<AnimateSharedLayout>
+								{pages.map((item, index) => (
+									<SiteItem
+										icon={item.icon}
+										title={item.title}
+										isSelected={index === openedSite}
+										key={index}
+										label={item.label}
+										username={username}
+									/>
+								))}
+							</AnimateSharedLayout>
+						</List>
+					</motion.div>
 				</Grid>
-			</Paper>
-		</>
+
+				<Grid item xs={12} sm={8} md={9}>
+					<div
+						style={{
+							width: "100%",
+							minHeight: "100%",
+							position: "relative",
+						}}
+					>
+						{pages[openedSite].content()}
+					</div>
+				</Grid>
+			</Grid>
+		</div>
 	);
 }
 
@@ -61,4 +272,4 @@ PublicProfile.propTypes = {
 	setTitle: PropTypes.func.isRequired,
 };
 
-export default connect(null, { setTitle })(PublicProfile);
+export default connect(null, { setTitle })(withRouter(PublicProfile));
